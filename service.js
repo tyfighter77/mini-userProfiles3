c3e511f8fb20d95a14a645877532103aff38b789
Ty Hill <crazysnoheadman@yahoo.com>
2015-12-17 12:29:04 -0700

    jeremy's code

--- service.js
+++ service.js
@@ -1,12 +1,22 @@
 var app = angular.module('userProfiles');
 
-app.service('mainService', function($http) {
+app.service('mainService', function($http, $q) {
 
   this.getUsers = function() {
-    return $http({
-        method: 'GET',
-        url: 'http://reqres.in/api/users?page=1'
-    })
-  }
+    var deferred = $q.defer();
+
+    $http({
+      method: 'GET',
+      url: 'http://reqres.in/api/users?page=1'
+    }).then(function(response) {
+      var data = response.data.data;
+      for(var i = 0; i < data.length; i++){
+        data[i].first_name = 'Skywalker';
+      }
+      deferred.resolve();
+    });
+
+    return deferred.promise;
+  };
 
 });
